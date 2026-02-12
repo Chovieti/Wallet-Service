88436ffd9ed8d9d7e4d8a75faa80bc342643c22b
chovie tinkerer <chovieti@un-j1.kzn.21-school.ru>
2026-02-12 19:48:45 +0300

    Подключение liquidbase

--- src/main/java/com/example/wallet_service/model/Wallet.java
+++ src/main/java/com/example/wallet_service/model/Wallet.java
@@ -3,11 +3,13 @@ package com.example.wallet_service.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
+@Table(name = "wallets")
 public class Wallet {
     @Id
     private UUID id;
